18edf6f84920b9cd29b160cee2dba201bb97124d
Rina Kumari <rinaraaj03@gmail.com>
2021-07-30 12:40:27 +0530

    final update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require("./config/db");
+const path = require('path');
+
 const app = express();
 //const bodyParser = require('body-parser');
 
@@ -13,7 +15,7 @@ app.use(express.json());
 
 //app.use(bodyParser.json())
 
-app.get("/", (req, res) => res.send("Eteam Appplication works!! ") );
+//app.get("/", (req, res) => res.send("Eteam Appplication works!! ") );
 
 // Define Routes
 
@@ -25,6 +27,17 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/employer', require('./routes/api/employer'));
 app.use('/api/empprofile', require('./routes/api/empprofile'));
 app.use('/api/job', require('./routes/api/job'));
+
+//Server static assests in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'))
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+
+    })
+}
 
 
 
